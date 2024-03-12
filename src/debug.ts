fa7a48381fa1c2e5f9c9c1897e3d2471681e09ac
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-03-12 11:23:39 -0700

    strip logs in production builds

--- src/debug.ts
+++ src/debug.ts
@@ -1,10 +1,17 @@
-// Debug Tool
-export function curryLog(initialText: string) {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return (...args: any[]) => console.log(initialText, ": ", ...args);
+// CurryLog is a way to add tagged logging that is stripped in production
+
+let curryLog: (initialText: string) => (...args: any[]) => void;
+
+if (process.env.NODE_ENV === "production") {
+	curryLog = (initialText: string) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return (...args: any[]) => {};
+	};
+} else {
+	curryLog = (initialText: string) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return (...args: any[]) => console.log(initialText, ": ", ...args);
+	};
 }
 
-//export function curryLog(initialText: string) {
-//	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-//	return (...args: any[]) => {};
-//}
+export { curryLog };
